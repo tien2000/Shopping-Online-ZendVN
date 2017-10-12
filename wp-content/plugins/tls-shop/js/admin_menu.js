46c7af032f97fd9fa85d6bba4289e62704132e18
tien <tienls@fastboy.net>
2017-10-12 18:44:44 +0700

    Custom Admin Menu || Video 04 | Edit admin_menu.js, removeClass, addClass for menu Product, Category.

---
+++ wp-content/plugins/tls-shop/js/admin_menu.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($){
+	console.log('admin-menu.js');
+	var elemt = ('#toplevel_page_tls-sp-manager');
+	$(elemt).removeClass('wp-not-current-submenu')
+				.addClass('wp-has-current-submenu wp-menu-open');
+	$(elemt + ' > a').removeClass('wp-not-current-submenu')
+				.addClass('wp-has-current-submenu wp-menu-open');
+	
+	var taxonomy = getURLParameter('taxonomy');
+	var post_type = getURLParameter('post_type');
+	if(post_type == 'tsproduct'){
+		if(taxonomy == 'ts-category'){
+			var url = 'edit-tags.php?taxonomy=ts-category&post_type=tsproduct';
+			$(elemt + " a[href='"+ url +"']").addClass('current')
+				.parent().addClass('current');
+		}
+		
+		if(taxonomy == undefined){
+			var url = 'edit.php?post_type=tsproduct';
+			$(elemt + " a[href='"+ url +"']").addClass('current')
+				.parent().addClass('current');
+		}
+	}
+	console.log(taxonomy);
+});
+
+function getURLParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++){
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam){
+            return sParameterName[1];
+        }
+    }
+}
