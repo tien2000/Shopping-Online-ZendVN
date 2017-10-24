2daa5265b28270f05eb0ae12b0a01e3d8f261aa2
tien <tienls@fastboy.net>
2017-10-24 17:50:24 +0700

    Edit Category.php

--- wp-content/plugins/tls-shop/helpers/Category.php
+++ wp-content/plugins/tls-shop/helpers/Category.php
@@ -6,7 +6,7 @@
         }
         
         public function test() {
-            echo '<br>' . __METHOD__;
-            global $wpdb;
+            /* echo '<br>' . __METHOD__;
+            global $wpdb; */
         }
     }
