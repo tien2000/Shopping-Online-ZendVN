b43e36e338147fe8f24d908ded76dc907e0799c8
tien <tienls@fastboy.net>
2017-11-01 17:53:54 +0700

    (FrontEnd) || Paging class | Pagination page.

---
+++ wp-content/plugins/tls-shop/helpers/Paging.php
@@ -0,0 +1,27 @@
+<?php
+class Tls_Sp_Paging_Helper{
+	
+	public function getPaging($queryObj = null){
+		if($queryObj != null){			
+			$big = 999999999;
+			//#038;
+			$pagenum_link = str_replace( $big, '%#%', get_pagenum_link( $big ));
+			$pagenum_link = str_replace( '#038;','&',  $pagenum_link);
+			
+			$args = array(
+					'base'               => $pagenum_link,
+					'format'             => '?page=%#%',
+					'total'              => $queryObj->max_num_pages,
+					'current'            => max(1,get_query_var('paged')),
+					'show_all'           => false,
+					'end_size'           => 1,
+					'mid_size'           => 2,
+					'prev_next'          => false,
+					'type'               => 'plain',
+			);
+			
+			return paginate_links($args);	
+			
+		}
+	}
+}
--- wp-content/plugins/tls-shop/templates/frontend/paging.php
+++ wp-content/plugins/tls-shop/templates/frontend/paging.php
@@ -1,10 +1,10 @@
 <?php
-	global $wpQuery, $zController;	
-	$paging  = $zController->getHelper('Paging');
+	global $wpQuery, $tController;	
+	$paging  = $tController->getHelper('Paging');
 
 ?>
 
 <div class="nav-article">
-	<span class="site-pagination-heading">Pages</span>
+	<span class="site-pagination-heading"><?php echo __('Pages: ');?></span>
 	<?php echo $paging->getPaging($wpQuery);?>	
 </div>
