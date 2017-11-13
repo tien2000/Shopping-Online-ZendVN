24c5cf9eb90528ee2ee6147843ac5803a10c2167
tien <tienls@fastboy.net>
2017-11-13 12:54:54 +0700

    Xây dựng hệ thống đặt hàng || Send mail 3-5

--- wp-content/plugins/tls-shop/controllers/frontend/Cart.php
+++ wp-content/plugins/tls-shop/controllers/frontend/Cart.php
@@ -1,3 +1,12 @@
+<?php 
+    /* 
+     * wp_mail(): Sử dụng phương thức mail() của PHP, đa số các host không cho sử dụng phương thức mail(),
+     *            phải gửi thông qua 1 địa chỉ email nào đó trên Hosting.
+     *            
+     * PHPMailer(): Đối tượng dụng cho mail.
+     *  */
+?>
+
 <?php
     class Tls_Sp_Cart_Controller{
         
@@ -27,6 +36,8 @@
                 case 'send_mail'   : $this->send_mail(); break;
                 case 'paypal'      : $this->paypal(); break;
                 
+                case 'alert'      : $this->alert(); break;
+                
                 default: $this->display(); break;
             }
         }
@@ -41,15 +52,120 @@
         public function send_mail(){
             //echo '<br>' . __METHOD__;
             
-            global $tController;
+            global $tController, $tls_sp_settings;
             
             $type = $tController->getParams('type');
             if ($type == 'send'){
                 // Kiểm tra dữ liệu có hợp lệ hay ko (Viết hàm Validate)
+                
+                /* echo '<pre>';
+                print_r($tController->getParams());
+                echo '</pre>';
+                
+                echo '<pre>';
+                print_r($tls_sp_settings);
+                echo '</pre>'; */
+                
+            // Lấy ra sản phẩm của KH đã mua
+                $tlsSs = $tController->getHelper('Session');
+                $mailHTML = $tlsSs->get('tcart_mail', '');
+                
+                $content = 'We have received your order. 
+    						We\'ll get back to you within 24 hours. 
+    						Thank you very much<br/>';
+    			$content .= '<br><h3>Your information';
+    			$content .= '<br/>===============================</h3>';
+    			$content .= 'Full name: ' 			    . $tController->getParams('full_name');
+    			$content .= '<br/>Phone: ' 				. $tController->getParams('phone');
+    			$content .= '<br/>Email: ' 				. $tController->getParams('email');
+    			$content .= '<br/>Bill address: ' 		. $tController->getParams('bill_address');
+    			$content .= '<br/>Shipping address: ' 	. $tController->getParams('shipping_address');
+    			$content .= '<br/>Comment: ' 			. $tController->getParams('comment');			
+    			$content .= '<br><br><h3>Your cart';
+    			$content .= '<br/>===============================</h3>';
+    			$content .=  '<div id="zendvn_sp_cart_table">' . $mailHTML . '</div>';
+    			
+    			//echo $content;
+    			
+    			$flagSend = false;
+                
+                if ($tls_sp_settings['select_type'] == 'system'){
+                    $flagSend = $this->mail_system($content);
+                } else {
+                    $flagSend = $this->mail_shopping($content);
+                }
+                
+                if ($flagSend == true){
+                    // Lưu vào DB.
+                }
+                
+                $url = site_url('?page_id=' . get_query_var('page_id') . '&action=alert&mes=' . $flagSend);
+                //wp_redirect($url);
             }
             
-            $tController->getView('display.php', 'frontend/cart');
-            $tController->getView('sent_mail.php', 'frontend/cart');
+            /* $tController->getView('display.php', 'frontend/cart');
+            $tController->getView('sent_mail.php', 'frontend/cart'); */
+        }
+        
+        public function alert(){
+            global $tController;
+            
+            $mes = $tController->getParams('mes');
+            
+            if($mes == 1){
+                echo '<div>Đơn đặt hàng của bạn đã được gửi. Chúng tôi sẽ liên lạc lại với bạn sớm nhất.</div>';
+            } else {
+                echo '<div>Hệ thống đang có vấn đề. Vui lòng thử lại sau.</div>';
+            }
+        }
+        
+        public function mail_system($content = null){
+            //echo '<br>' . __METHOD__;
+            
+            global $tController, $tls_sp_settings;
+            
+            $to             = $tController->getParams('email');
+            $subject        = 'Dead ' . $tController->getParams('full_name') . '! From TShopping ';
+            $message        = $content;
+            $headers[]      = 'Content-Type: text/html; charset=UTF-8';
+            $headers[]      = 'From: tShopping <no-reply@abc.com>';
+            $headers[]      = 'Cc: TienLe <tienls6589@gmail.com>';
+            $headers[]      = 'Bcc: TienLe <'. $tls_sp_settings['alert_to_email'] .'>';            
+            $attachments[]  = TLS_SP_PUBLIC_PATH . '/files/contract.zip';
+            
+            return wp_mail($to, $subject, $message, $headers, $attachments);    // Trả về 2 giá trị true/false, true: gửi được mail, false: không gửi được mail.
+        }
+        
+        public function mail_shopping($content = null){
+            echo '<br>' . __METHOD__;
+            
+            global $tController, $tls_sp_settings;
+            
+            require_once ABSPATH . WPINC . '/class-phpmailer.php';
+            require_once ABSPATH . WPINC . '/class-smtp.php';
+            
+            $mail = new PHPMailer();
+            
+            $mail->isSMTP();            
+            $mail->SMTPDebug = 1;            
+            
+            $mail->SMTPAuth     = true;
+            $mail->SMTPSecure   = $tls_sp_settings['encription'];
+            $mail->Host         = $tls_sp_settings['smtp_host'];
+            $mail->Port         = $tls_sp_settings['smpt_port'];
+            $mail->Username     = $tls_sp_settings['smtp_username'];
+            $mail->Password     = $tls_sp_settings['smtp_password'];
+            
+            $mail->setFrom('neotien200065@gmail.com', 'tShopping');
+            $mail->addAddress($tController->getParams('email'), $tController->getParams('full_name'));
+            $mail->addReplyTo($tController->getParams('alert_to_email'));
+            $mail->addAttachment(TLS_SP_PUBLIC_PATH . '/files/contract.zip');
+            
+            $mail->Subject = 'Dead ' . $tController->getParams('full_name') . '! From TShopping ';
+            $mail->CharSet = 'utf-8';
+            $mail->msgHTML($content);
+            
+            return $mail->send();
         }
         
         public function paypal(){
